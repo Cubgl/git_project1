bd550d596775dfadac00b1e159fcf4ad5408c8b1
Cubgl <74872839+Cubgl@users.noreply.github.com>
2020-11-23 00:06:16 +0500

    Update program.py

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('This is new experience!')
+    print('Hello!')
 
 if __name__ == '__main__':
     main()
